fbb94fce98a9a68b2864a01d9d2a9315e314b6d9
Alfred Sjöholm <alfsjo0116@skynet.halmstad.se>
2019-11-14 12:41:49 +0100

    test

---
+++ users.js
@@ -0,0 +1,6 @@
+
+module.exports = [
+    {id:1, email:"lars@lasse.se", password:"$2a$12$2e.S9.FghsP1LOdgGO7LIOvMWqX24iSsgoRcqSiK.5eUFmiRMrnZC"},
+    {id:2, email:"lena@larsson.se", password:"$2a$12$2e.S9.FghsP1LOdgGO7LIOvMWqX24iSsgoRcqSiK.5eUFmiRMrnZC"},
+    {id:3, email:"jens@peter.se", password:"$2a$12$PV7NYwAWNNdkGyt9gxXXcO391/DaKBDk99oBYTQ6sTGzh2V5hw.ta"}
+];
